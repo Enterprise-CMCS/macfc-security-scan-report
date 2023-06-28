993b49f1d0035c6e049ef3ddfdbbb7acd0f4696b
chapi <chapi@chapis-MacBook-Pro.local>
2023-06-28 12:06:17 -0400

    updated snyk parser

--- src/index.js
+++ src/index.js
@@ -124,7 +124,7 @@ try {
       if (inputData) {
         try {
           const data = JSON.parse(inputData);
-          for (const project of data) {
+          for (const project of Object.values(data)) {
             vulnerabilities = vulnerabilities.concat(project.vulnerabilities);
           }
         } catch (error) {
